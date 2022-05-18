616041a7566aed9abaf02e434ea4f5dc14d073dc
EEEEnusya <yafilatovaa@yandex.ru>
2022-05-18 09:47:42 +0300

    push

---
+++ Bible/kub.h
@@ -0,0 +1,10 @@
+//
+// Created by yana on 18.05.2022.
+//
+
+#ifndef LAB07_KUB_H
+#define LAB07_KUB_H
+
+void kub(int a);
+
+#endif //LAB07_KUB_H
---
+++ source/kub.cpp
@@ -0,0 +1,10 @@
+//
+// Created by yana on 18.05.2022.
+//
+
+#include <kub.h>
+void kub(int a){
+    int b=a;
+    a=a*a;
+    a= b*a;
+}
